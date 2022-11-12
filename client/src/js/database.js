2f6d40536e666bf91d21a6853fc410cdce8e1288
Laura Kambanis <kambanislaura@gmail.com>
2022-11-12 19:07:09 +0000

    changes webpack config

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,7 +17,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error("putDb not implemented");
+  console.log("putDb implemented");
 
   //create a connection to the database and version want to use
   const jateDb = await openDB("jate", 1);
@@ -29,18 +29,18 @@ export const putDb = async (content) => {
 
   const store = tx.objectStore("jate");
 
-  //use .add() method on the store and pass in the content.
-  const request = store.add({ content });
+  //use .add() method on the store and pass in the content. //store.put?
+  const request = store.put({ id: 1, value: content });
 
   //get confirmation of the request
 
   const result = await request;
-  console.log("data saved to the database", result);
+  console.log("data saved to the database", result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error("getDb not implemented");
+  console.log("getDb implemented");
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB("jate", 1);
 
@@ -51,11 +51,11 @@ export const getDb = async () => {
   const store = tx.objectStore("jate");
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log("result.value", result);
-  return result;
+  return result.value;
 };
 initdb();
